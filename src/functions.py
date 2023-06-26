f193f77ca7ce17e6137a3b144a0a2f4987eb4207
aiyumrin <aiyumrin@gmail.com>
2023-06-26 13:56:17 +0500

    Add functions

--- src/functions.py
+++ src/functions.py
@@ -1,13 +1,17 @@
-def auth():
-      import os
-      from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-      from selenium import webdriver
-      import pickle
-      import time
-      import random
-      from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+import time
+import re
+import pickle
+import os
+import random
+import pandas as pd
 
 
+def auth(driver):
       #if __name__ == '__main__':
 
       #Логин
@@ -22,10 +26,6 @@ def auth():
       #Проверка наличия папки для cookies
       if not os.path.exists(FILES_PATH):
             os.mkdir(FILES_PATH)
-
-      caps = DesiredCapabilities().CHROME
-      caps['pageLoadStrategy'] = 'eager'
-      driver = webdriver.Chrome()
 
       #Авторизация по cookies
       if os.path.exists(FILES_PATH + COOKIES_PATH):
@@ -49,4 +49,61 @@ def auth():
             driver.find_element(By.XPATH, "//button[@type='submit']").click()
             time.sleep(10) #время на ввод кода подтверждения если понадобится
             pickle.dump(driver.get_cookies(), open(FILES_PATH + COOKIES_PATH, 'wb'))
-      return driver
+
+def profile_urls(driver, search_url):
+      #Загрузка страницы с результатами поиска
+      driver.get(search_url)
+      time.sleep(random.uniform(.5, 3))
+
+      profile_urls = []
+      NUM_PAGES_TO_PARSE = 1
+
+      for i in range(NUM_PAGES_TO_PARSE):
+
+            print('Page', i+1)
+
+            search_result_links = driver.find_elements(By.CSS_SELECTOR, "span.entity-result__title-text a.app-aware-link")
+
+            for link in search_result_links:
+                  href = link.get_attribute("href")
+                  if 'linkedin.com/in' in href:
+                        profile_urls.append(href)
+
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            NUM_SCROLLS = 5
+
+            for j in range(NUM_SCROLLS):
+                  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                  time.sleep(random.uniform(1.5, 3))
+                  new_height = driver.execute_script("return document.body.scrollHeight")
+                  if new_height == last_height:
+                        break
+                  last_height = new_height
+
+            time.sleep(random.uniform(1, 2))
+                  
+            print('Sucsess!')
+            print('-' * 50)
+
+            # k = 1
+            # errors = 0
+            # while k == 1 or errors < 10:
+                  
+            #     try:
+            #         next_button = driver.find_element(By.CSS_SELECTOR, 'button.artdeco-pagination__button--next')
+            #         next_button.click()
+            #         k = 2
+            #     except: 
+            #         print(f'ERROR on page {i+1}! {errors}')
+            #         errors += 1
+
+            # time.sleep(random.uniform(.5, 1))
+
+
+      profile_urls = list(set(profile_urls))
+      print('Done!')
+
+      urls = pd.DataFrame(profile_urls, columns=['profile_url'])
+      urls['profile_url'] = urls['profile_url'].apply(lambda x: x.split('?')[0])
+      print(urls)
+      urls.to_csv('profile_urls.csv', index=False)
--- src/main.py
+++ src/main.py
@@ -11,65 +11,36 @@ import random
 import pandas as pd
 
 from functions import auth
+from functions import profile_urls
 
-driver = auth()
+caps = DesiredCapabilities().CHROME
+caps['pageLoadStrategy'] = 'eager'
+driver = webdriver.Chrome()
+SEARCH_URL = 'https://www.linkedin.com/search/results/people/?keywords=data%20scientist&origin=CLUSTER_EXPANSION&sid=1gy'
 
-#Загрузка страницы с результатами поиска
-driver.get('https://www.linkedin.com/search/results/people/?keywords=data%20scientist&origin=CLUSTER_EXPANSION&sid=1gy')
-time.sleep(random.uniform(.5, 3))
-
-profile_urls = []
-NUM_PAGES_TO_PARSE = 1
-
-for i in range(NUM_PAGES_TO_PARSE):
-
-    print('Page', i+1)
-
-    search_result_links = driver.find_elements(By.CSS_SELECTOR, "span.entity-result__title-text a.app-aware-link")
-
-    for link in search_result_links:
-        href = link.get_attribute("href")
-        if 'linkedin.com/in' in href:
-            profile_urls.append(href)
-
-    last_height = driver.execute_script("return document.body.scrollHeight")
-    NUM_SCROLLS = 10
-
-    for j in range(NUM_SCROLLS):
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(random.uniform(1.5, 3))
-        new_height = driver.execute_script("return document.body.scrollHeight")
-        if new_height == last_height:
-            break
-        last_height = new_height
-
-    time.sleep(random.uniform(1, 2))
-            
-    print('Sucsess!')
-    print('-' * 50)
-
-        # k = 1
-        # errors = 0
-        # while k == 1 or errors < 10:
-            
-        #     try:
-        #         next_button = driver.find_element(By.CSS_SELECTOR, 'button.artdeco-pagination__button--next')
-        #         next_button.click()
-        #         k = 2
-        #     except: 
-        #         print(f'ERROR on page {i+1}! {errors}')
-        #         errors += 1
-
-        # time.sleep(random.uniform(.5, 1))
+auth(driver)
+profile_urls(driver, SEARCH_URL)
 
 
-    profile_urls = list(set(profile_urls))
-    print('Done!')
 
-    urls = pd.DataFrame(profile_urls, columns=['profile_url'])
-    urls['profile_url'] = urls['profile_url'].apply(lambda x: x.split('?')[0])
-    print(urls)
-    urls.to_csv('profile_urls.csv', index=False)
+
+
+
+
+
+
+
+
+driver.get()
+time.sleep(20)
+urls = pd.read_csv('profile_urls.csv')
+print(urls)
+
+for url in urls['profile_url']:
+    driver.get(url)
+    time.sleep(random.uniform(20, 30))
+
+
 
     # for profile_url in profile_urls:
     #     #get_and_print_profile_info(driver, profile_url)
