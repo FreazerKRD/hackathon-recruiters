305c3a4cefff621b34bc76895684e9d42d7678d7
aiyumrin <aiyumrin@gmail.com>
2023-06-28 17:35:16 +0500

    Parser

--- src/functions.py
+++ src/functions.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import NoSuchElementException
 
 import time
 import re
@@ -9,29 +10,21 @@ import pickle
 import os
 import random
 import pandas as pd
+import csv
 
 
-def auth(driver):
-      #if __name__ == '__main__':
-
-      #Логин
-      USER_LOGIN = 'fedorfedukov79@gmail.com'
-
+def auth(driver, login, files_path):
       #Название файла cookies
-      COOKIES_PATH = '\lincookies' + str(USER_LOGIN.split('@')[0])
-
-      #Папка для хранения cookies
-      FILES_PATH = 'D:\Recruiters'
-
+      COOKIES_PATH = '\lincookies' + str(login.split('@')[0])
       #Проверка наличия папки для cookies
-      if not os.path.exists(FILES_PATH):
-            os.mkdir(FILES_PATH)
+      if not os.path.exists(files_path):
+            os.mkdir(files_path)
 
       #Авторизация по cookies
-      if os.path.exists(FILES_PATH + COOKIES_PATH):
+      if os.path.exists(files_path + COOKIES_PATH):
             driver.get('https://linkedin.com')
             #Загрузка куки
-            for cookie in pickle.load(open(FILES_PATH + COOKIES_PATH, 'rb')):
+            for cookie in pickle.load(open(files_path + COOKIES_PATH, 'rb')):
                   driver.add_cookie(cookie)
             time.sleep(random.uniform(.5, 1))
             driver.refresh()
@@ -43,12 +36,12 @@ def auth(driver):
             #Авторизация
             time.sleep(random.uniform(2, 4))
             username = driver.find_element(By.ID, "username")
-            username.send_keys(USER_LOGIN)
+            username.send_keys(login)
             pword = driver.find_element(By.ID, "password")
             pword.send_keys(str(input('Введите пароль: ')))
             driver.find_element(By.XPATH, "//button[@type='submit']").click()
             time.sleep(30) #время на ввод кода подтверждения если понадобится
-            pickle.dump(driver.get_cookies(), open(FILES_PATH + COOKIES_PATH, 'wb'))
+            pickle.dump(driver.get_cookies(), open(files_path + COOKIES_PATH, 'wb'))
 
 def profile_urls(driver, search_url):
       #Загрузка страницы с результатами поиска
@@ -108,7 +101,7 @@ def profile_urls(driver, search_url):
       print(urls)
       urls.to_csv('profile_urls.csv', index=False)
 
-def get_and_print_users_posts(driver, url):
+def get_and_save_users_posts(driver, url, posts_file_name):
       from csv import writer
       driver.get(url + '/recent-activity/all/')
       time.sleep(random.uniform(.5, 1))
@@ -207,7 +200,154 @@ def get_and_print_users_posts(driver, url):
             else:
                  comment_cnt = 0 
             print(f'Comments: {comment_cnt}')
-            post_source.append(comment_cnt)          
-            with open('posts.csv', 'a', newline='', encoding='utf-8') as file:
+            post_source.append(comment_cnt)
+
+            with open(posts_file_name, 'a', newline='', encoding='utf-8') as file:
                   writer_obj = writer(file)
                   writer_obj.writerow(post_source)
+
+            time.sleep(random.uniform(2, 5))
+    
+      # Возврат на страницу профиля
+      driver.execute_script("window.history.go(-1)")
+
+def get_and_save_profile_info(driver, profile_url, info_file_name, posts_file_name):
+      info_source = []
+    
+      # This will open the link
+      driver.get(profile_url)
+    
+      # Random sleeping time to load all data
+      time.sleep(random.uniform(5, 7))
+
+      # Extracting data from page with BeautifulSoup
+      src = driver.page_source
+
+      # Now using beautiful soup    
+      soup = BeautifulSoup(src, 'lxml')
+
+      # Extracting the HTML of the complete introduction box
+      # that contains the name, status, and the location
+      intro = soup.find('div', {'class': 'pv-text-details__left-panel'})
+    
+      # Extracting the Name
+      # In case of an error, try changing the tags used here.
+      name_loc = intro.find("h1")
+      # strip() is used to remove any extra blank spaces
+      name = name_loc.get_text().strip()
+      info_source.append(name)
+
+      # This gives us the HTML of the tag in which user status is present
+      status_loc = intro.find("div", {'class': 'text-body-medium'})
+      # Extracting user status
+      status = status_loc.get_text().strip()
+      info_source.append(status)
+      
+      try:
+            # Extracting the Company name
+            work_space = soup.find('ul', {'class': 'pv-text-details__right-panel'})
+            # This gives us the HTML of the tag in which the Company Name is present
+            works_at_loc = work_space.find('span', {'class': 'pv-text-details__right-panel-item-text'})
+            # Extracting the Company Name
+            works_at = works_at_loc.get_text().strip()
+            info_source.append(works_at)
+      except: 
+            info_source.append('n/a')
+
+      # Добавил случайный скроллинг страницы для имитации человека
+      last_height = driver.execute_script("return document.body.scrollHeight")
+      NUM_SCROLLS = random.randint(4, 10)
+
+      for _ in range(NUM_SCROLLS):
+            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(random.uniform(1.5, 3))
+            new_height = driver.execute_script("return document.body.scrollHeight")
+            if new_height == last_height:
+                  break
+            last_height = new_height
+
+      time.sleep(random.uniform(2, 5))
+    
+      # Add user profile URL
+      info_source.append(profile_url)
+    
+      # Сохранение в файл
+      with open(info_file_name, 'a', newline='', encoding='utf-8') as file:
+            writer_obj = csv.writer(file)
+            writer_obj.writerow(info_source)
+        
+      # Собираем посты текущего пользователя, если они существуют
+      by = By.CSS_SELECTOR
+      path = 'li.profile-creator-shared-feed-update__mini-container'
+      # Проверка существования элемента на странице
+      def check_exists_element(by, path):
+            try:
+                  driver.find_element(by, path)
+            except NoSuchElementException:
+                  return False
+            return True
+      
+      if check_exists_element(by, path):
+            get_and_save_users_posts(driver, profile_url, posts_file_name)
+        
+      time.sleep(random.uniform(1, 3))
+      
+      # Возврат на страницу поиска
+      driver.execute_script("window.history.go(-1)")
+
+def search_pages(driver, num_pages, info_file_name, posts_file_name):
+      for i in range(num_pages):
+            # Задержка для полной загрузки страницы
+            time.sleep(random.uniform(4, 7))
+
+            last_height = driver.execute_script("return document.body.scrollHeight")
+            NUM_SCROLLS = 20
+
+            for _ in range(NUM_SCROLLS):
+                  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                  time.sleep(random.uniform(1.5, 3))
+                  new_height = driver.execute_script("return document.body.scrollHeight")
+                  if new_height == last_height:
+                        break
+                  last_height = new_height
+
+            search_result_links = driver.find_elements(By.CSS_SELECTOR, "span.entity-result__title-text a.app-aware-link")
+
+            profile_urls = []
+            
+            for link in search_result_links:
+                  href = link.get_attribute("href")
+                  if 'linkedin.com/in' in href:
+                        profile_urls.append(href)
+                  
+            profile_urls = pd.Series(profile_urls)
+            profile_urls_cut = profile_urls.str.split('?', n=1).str[0]
+            
+            time.sleep(random.uniform(2, 5))
+            
+            # Парсинг информации о пользователях и их постах
+            # Создаем файл для записи инфо о пользователях если он не существует
+            if not os.path.exists(info_file_name):
+                  header = ['name', 'status', 'company', 'profile_url']
+                  with open(info_file_name, 'w', newline='', encoding='utf-8') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(header)
+                  
+            #Создаем файл для записи постов если он не существует
+            if not os.path.exists(posts_file_name):
+                  header = ['url', 'text', 'likes_cnt', 'reposts_cnt', 'comments_cnt']
+                  with open(posts_file_name, 'w', newline='', encoding='utf-8') as file:
+                        writer = csv.writer(file)
+                        writer.writerow(header)
+
+            # Получение информации из профилей пользователей
+            for url in profile_urls_cut:
+                  get_and_save_profile_info(driver, url, info_file_name, posts_file_name)
+                  time.sleep(random.uniform(55, 75))
+
+            print('Current search page completly parsed:')
+            print(driver.current_url)
+            print('- ' * 25)
+
+            next_button = driver.find_element(By.CSS_SELECTOR, 'button.artdeco-pagination__button--next')
+            next_button.click()
--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.common.exceptions import NoSuchElementException
 
 import time
 import re
@@ -14,38 +15,44 @@ import csv
 
 import functions
 
+#Ссылка на поиск
+SEARCH_URL = 'https://www.linkedin.com/search/results/people/?currentCompany=%5B%2210718%22%2C%22579461%22%2C%22970369%22%2C%2219201%22%2C%22868912%22%2C%226132%22%2C%2218326534%22%2C%228979%22%2C%2212611%22%2C%222831%22%2C%2210876016%22%2C%2212650056%22%2C%22134818%22%2C%2219031362%22%2C%2232642%22%2C%223735315%22%2C%2266724050%22%2C%2279365269%22%2C%22918215%22%5D&geoUrn=%5B%22106686604%22%5D&industry=%5B%221594%22%2C%226%22%2C%2296%22%2C%224%22%5D&origin=FACETED_SEARCH&sid=LiV'
+#Логин для входа на linkedin
+USER_LOGIN = 'fedorfedukov79@gmail.com'
+#Папка для хранения файлов
+FILES_PATH = 'D:\Recruiters'
+# Количество страниц результатов поиска для парсинга
+NUM_PAGES_TO_PARSE = int(input('Введите количество страниц для парсинга: '))
+# Путь к файлу с постами
+POSTS_FILE_NAME = FILES_PATH + r"\posts.csv"
+# Путь к файлу с инфо пользователей
+INFO_FILE_NAME = FILES_PATH + r"\user-info.csv"
+# Путь к файлу с постами
+POSTS_FILE_NAME = FILES_PATH + r"\posts.csv"
+
 if __name__ == '__main__':
     #Инициализируем веб-драйвер
     caps = DesiredCapabilities().CHROME
     caps['pageLoadStrategy'] = 'eager'
     driver = webdriver.Chrome()
-    driver.implicitly_wait(30)
-    #Ссылка на поиск
-    SEARCH_URL = 'https://www.linkedin.com/search/results/people/?currentCompany=%5B%2210718%22%2C%2277009034%22%2C%22579461%22%2C%22868912%22%2C%22970369%22%5D&geoUrn=%5B%22106686604%22%5D&industry=%5B%221594%22%2C%226%22%2C%221810%22%2C%2296%22%2C%224%22%5D&origin=FACETED_SEARCH&page=3&sid=)q%3B'
+    driver.implicitly_wait(10)
+
+    # Установлен широкоформатный размер экрана, чтобы нужные элементы помещались на экране
+    driver.set_window_size(1920, 1080)
 
     #Вызов функции авторизации
-    functions.auth(driver)
-    time.sleep(random.uniform(.5, 1))
+    functions.auth(driver, USER_LOGIN, FILES_PATH)
+    time.sleep(10)
+
+    # Парсинг страниц поисковой выдачи
+    # Переход на стартовую страницу поиска
     driver.get(SEARCH_URL)
-    time.sleep(30)
 
+    # Вызов функции для парсинга
+    functions.search_pages(driver, 100, INFO_FILE_NAME, POSTS_FILE_NAME)
 
+    # Пауза
+    time.sleep(random.uniform(5, 12))
 
-
-
-# #Вызов функции сбора url
-# profile_urls(driver, SEARCH_URL)
-# time.sleep(random.uniform(.5, 1))
-
-# urls = pd.read_csv('profile_urls.csv')['profile_url']
-
-# #Создаем файл для записи постов
-# header = ['url', 'text', 'likes_cnt', 'reposts_cnt', 'comments_cnt']
-# with open('posts.csv', 'w', newline='', encoding='utf-8') as file:
-#     writer = csv.writer(file)
-#     writer.writerow(header)
-
-# for url in urls:
-#     get_and_print_users_posts(driver, url)
-
-# driver.quit()
+    # Выключаем драйвер скрэппинга
+    driver.quit()
