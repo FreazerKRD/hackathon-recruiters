db36d2314fa8d1ea0a71ab310c0880d0963e0ef1
aiyumrin <aiyumrin@gmail.com>
2023-06-28 14:38:13 +0500

    Functions

--- src/main.py
+++ src/main.py
@@ -10,21 +10,25 @@ import os
 import random
 import pandas as pd
 
-from functions import auth
-from functions import profile_urls
-from functions import get_and_print_users_posts
 import csv
 
-caps = DesiredCapabilities().CHROME
-caps['pageLoadStrategy'] = 'eager'
-driver = webdriver.Chrome()
-driver.implicitly_wait(30)
-#Ссылка на поиск
-SEARCH_URL = 'https://www.linkedin.com/search/results/people/?currentCompany=%5B%2210718%22%2C%2277009034%22%2C%22579461%22%2C%22868912%22%2C%22970369%22%5D&geoUrn=%5B%22106686604%22%5D&industry=%5B%221594%22%2C%226%22%2C%221810%22%2C%2296%22%2C%224%22%5D&origin=FACETED_SEARCH&page=3&sid=)q%3B'
+import functions
 
-#Вызов функции авторизации
-auth(driver)
-time.sleep(random.uniform(.5, 1))
+if __name__ == '__main__':
+    #Инициализируем веб-драйвер
+    caps = DesiredCapabilities().CHROME
+    caps['pageLoadStrategy'] = 'eager'
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
+    #Ссылка на поиск
+    SEARCH_URL = 'https://www.linkedin.com/search/results/people/?currentCompany=%5B%2210718%22%2C%2277009034%22%2C%22579461%22%2C%22868912%22%2C%22970369%22%5D&geoUrn=%5B%22106686604%22%5D&industry=%5B%221594%22%2C%226%22%2C%221810%22%2C%2296%22%2C%224%22%5D&origin=FACETED_SEARCH&page=3&sid=)q%3B'
+
+    #Вызов функции авторизации
+    functions.auth(driver)
+    time.sleep(random.uniform(.5, 1))
+    driver.get(SEARCH_URL)
+    time.sleep(30)
+
 
 
 
